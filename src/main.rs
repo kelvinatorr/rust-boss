f29b5a3c037e0bcf27ecc038dafa00d160ef2455
Kelvin Dealca <dealca@fsi.io>
2025-07-16 16:18:32 -0700

    Add parsing arguments into correct types and custom error

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
-use std::{env, process};
+use std::error::Error;
+use std::fmt;
+use std::{env, num::ParseIntError, process, str::ParseBoolError};
+
+#[derive(Debug)]
+struct ArgParseError {
+    msg: String,
+}
+impl fmt::Display for ArgParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Something went wrong!")
+    }
+}
+impl From<ParseIntError> for ArgParseError {
+    fn from(err: ParseIntError) -> Self {
+        ArgParseError {
+            msg: err.to_string(),
+        }
+    }
+}
+impl From<ParseBoolError> for ArgParseError {
+    fn from(err: ParseBoolError) -> Self {
+        ArgParseError {
+            msg: err.to_string(),
+        }
+    }
+}
+impl Error for ArgParseError {}
 
 fn main() {
     println!("Rust Implementation Of Interval Boss");
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        println!("Problem parsing arguments: {}", err.msg);
         process::exit(1);
     });
 
@@ -18,28 +45,30 @@ fn main() {
     run(config);
 }
 
-fn run(config: Config) {
+fn run(_config: Config) {
     println!("Running!");
 }
 
 struct Config {
-    get_ready: String,
-    first_timer: String,
-    second_timer: String,
-    rounds: String,
-    one_shot: String,
+    get_ready: u32,
+    first_timer: u32,
+    second_timer: u32,
+    rounds: u16,
+    one_shot: bool,
 }
 
 impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
+    fn build(args: &[String]) -> Result<Config, ArgParseError> {
         if args.len() < 6 {
-            return Err("not enough arguments");
+            return Err(ArgParseError {
+                msg: String::from("not enough arguments"),
+            });
         }
-        let get_ready = args[1].clone();
-        let first_timer = args[2].clone();
-        let second_timer = args[3].clone();
-        let rounds = args[4].clone();
-        let one_shot = args[5].clone();
+        let get_ready: u32 = args[1].trim().parse()?;
+        let first_timer: u32 = args[2].trim().parse()?;
+        let second_timer: u32 = args[3].trim().parse()?;
+        let rounds: u16 = args[4].trim().parse()?;
+        let one_shot: bool = args[5].trim().parse()?;
 
         Ok(Config {
             get_ready,
